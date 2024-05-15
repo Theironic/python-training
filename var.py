33438e291c38bef153a48cc84a8b84617a507515
Lucca Souza <theironic117@proton.me>
2024-05-15 19:54:26 -0300

    commit 0.2

--- var.py
+++ var.py
@@ -1,4 +1,17 @@
-name = "your name";
-num = 0;
-dot = 0.0;
+name = "your name"; #string
+num = 0; #int
+dot = 0.0 #float
+name2, age = "your name", 0 #string and int
+real = True # boolean value
+
 print(name);
+print(dot, num,name2 )
+
+#type funcion
+print(type(dot)) # output will be float
+print(type(name))# output will be string
+print(type(age)) # output will be int
+print(type(real)) # output will be boolean(true or false)
+print(type(1+4j)) # output will be complex
+
+#isinstance funcion
