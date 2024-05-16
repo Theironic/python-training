2f8ac792fc8e546a0eb5e736f7c3693a4fd111c1
Lucca Souza <theironic117@proton.me>
2024-05-16 16:24:40 -0300

    commit 0.4.1

--- operators.py
+++ operators.py
@@ -1,3 +1,18 @@
 x = y = z = 0
 
 print(x, y,z)
+# Addition And Subtraction
+a = 10
+print(a + a)
+print(a - a)
+# potentiation, multiplication, division and int division
+b = 15
+c = 5
+print(c ** c)
+print(c * c)
+print(b / c)
+print(b // c)
+# modulus
+x,y = 10 , 2
+print(x % y)
+
