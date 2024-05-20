d5339d51edc43ef4014718910e968c2ac2a30aeb
Lucca Souza <theironic117@proton.me>
2024-05-20 12:18:11 -0300

    commit 0.5.5

--- logical-operators.py
+++ logical-operators.py
@@ -3,4 +3,28 @@ height = 1.75
 
 result = (age >= 18) and (height >=1.75)
 
-print(result)
+
+output = "Can you get in the car ? " + str(result) 
+print(output, "\n")
+# output will be True
+print("test 2 \n")
+
+age = 17
+height = 1.75
+
+result = (age >= 18) and (height >=1.75)
+
+output = "Can you get in the car ? " + str(result)
+print(output ,"\n")
+# output will be Falte
+
+# logical or
+
+#the algorithm will check whether the door **or** windows are closed
+
+door = "c"
+window = "c"
+
+alarm = (door == "o") or (window == "o")
+output = "alarm activated ?" + str(alarm)
+print(output, "\n")
