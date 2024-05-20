c951329d6fc890e661c4c24a60839e411f9e3c0c
Lucca Souza <theironic117@proton.me>
2024-05-20 11:51:52 -0300

    commit 0.5.4

---
+++ logical-operators.py
@@ -0,0 +1,6 @@
+age = 25
+height = 1.75
+
+result = (age >= 18) and (height >=1.75)
+
+print(result)
---
+++ relacional-operators.py
@@ -0,0 +1,16 @@
+# var
+x = y = z = False
+num1 = num2 = 0
+# data input
+num1 = int(input("enter a value: "))
+num2 = int(input("enter a value: "))
+# variable x compare the value
+x = num2 == num1
+print("they are equal ?", x)
+# variable z checks if the value is greater
+z = num1 > num2
+print(num1, "num1 is greater than ", num2, '?', z )
+
+# variable z checks if the value is different
+y = num1 != num2
+print("are different ?", y)
