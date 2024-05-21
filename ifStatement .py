c7013a574ce6e9f0f706bf675a1e4df73e296e33
Lucca Souza <theironic117@proton.me>
2024-05-20 22:03:43 -0300

    commit 0.6.8

--- ifStatement .py
+++ ifStatement .py
@@ -6,5 +6,11 @@ average = 0.0
 num1 = float(input('enter with number:'))
 num2 = float(input('enter with number:'))
 
+# if the average is equal to or greater than seven, write approved
 if (num1 + num2) / 2 >= 7:
     print("you were approved")
+    # you can also create var media
+
+# otherwise, write disapproved
+else:
+    print("you were rejected")
