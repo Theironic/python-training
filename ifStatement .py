41d79183451f6c0c687ed1146b52434c9196a782
Lucca Souza <theironic117@proton.me>
2024-05-20 21:52:33 -0300

    commit 0.6.7

---
+++ ifStatement .py
@@ -0,0 +1,10 @@
+# if statementes 
+num1= num2 = 0.0
+average = 0.0
+
+
+num1 = float(input('enter with number:'))
+num2 = float(input('enter with number:'))
+
+if (num1 + num2) / 2 >= 7:
+    print("you were approved")
