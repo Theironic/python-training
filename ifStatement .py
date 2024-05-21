117d6ac4a83bfb820cc32d075489078b89282715
Lucca Souza <theironic117@proton.me>
2024-05-20 22:16:43 -0300

    commit  0.6.9

--- ifStatement .py
+++ ifStatement .py
@@ -1,16 +1,26 @@
 # if statementes 
 num1= num2 = 0.0
-average = 0.0
+
 
 
 num1 = float(input('enter with number:'))
 num2 = float(input('enter with number:'))
 
+
+# if statements
+if (num1 + num2) / 2 >= 7:
+    print(f"approved, yours {(num1 + num2) / 2} \n")
+
+# if else statements
+
 # if the average is equal to or greater than seven, write approved
 if (num1 + num2) / 2 >= 7:
-    print("you were approved")
+    print("you were approved \n")
     # you can also create var media
 
 # otherwise, write disapproved
 else:
     print("you were rejected")
+
+average = (num1 + num2) / 2
+# Python if elif else statement
