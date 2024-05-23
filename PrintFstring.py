9a9e53c83b872c3d2e4ef95b5eee36a026fee3f7
Lucca Souza <theironic117@proton.me>
2024-05-22 22:21:09 -0300

    commit 0.8.2

--- PrintFstring.py
+++ PrintFstring.py
@@ -1,4 +1,4 @@
-# 
+# way to show variables
 
 name = "jon smith"
 
@@ -7,3 +7,32 @@ name2 = 'ana evans'
 print("here have a name -",name)
 
 print(name, "-",name2)
+
+name = input("your name")
+
+print("welcome" + name + "!")
+
+#Python has a feature that automatically break the line after print
+
+#let's see how to disable this
+
+print('this line will be broken automatically')
+print('this line will not be automatically broken,', end='')
+print(' work!')
+
+# now you will see python .format
+
+name3 = 'joe'
+
+age = 47
+msg = "welcome {0} your age is {1}".format(name3,age)
+
+print (msg)
+
+# Now you will see what I use most, fstring
+print(f'testinf f string {name} and {name2}')
+
+# fstring with numbers
+x = 10
+y = 20
+print(f'x + y = {x + y}')
