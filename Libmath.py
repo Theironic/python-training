d565db632da581a3ef4065e64166f8c832e1b415
Lucca Souza <theironic117@proton.me>
2024-05-25 14:50:35 -0300

    commit 0.10.4

--- Libmath.py
+++ Libmath.py
@@ -4,7 +4,7 @@ import random
 val = random.randint(0,10) # choose a random number between 0-10
 
 print(val)
-##
+#############
 
 print('write fives numbers int')
 
@@ -17,3 +17,18 @@ print('here ! \n')
 for i in range(1,4,1):
     n = random.randint(0,50)
     print(f'another example {n} \n')
+
+# how to generate float value
+
+z = random.random()
+print('number: {0}'.format(z)) 
+print(f'float number {round(z * 10, 2)} \n')
+
+# another way
+x = random.uniform(1,400)
+print(f'look {x} \n')
+
+# choose a random number
+L = [1,2,3,4,5,6,7,8,9,10]
+giveaway = random.choice(L)
+print(f'random choice {giveaway}')
